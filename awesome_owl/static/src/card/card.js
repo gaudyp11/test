muhrizky/tutorials-owl

[Solution] 1.3 A simple Card component

owl_playground/static/src/card/card.js
@@ -1,0 +1,7 @@
+/** @odoo-module */
+
+import { Component } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "owl_playground.Card";
+}

gaudyp11/test

awesome_owl/static/src/card/card.js
@@ -1,0 +1,7 @@
+/** @odoo-module */
+
+import { Component } from "@odoo/owl";
+
+export class Card extends Component {
+    static template = "awesome_owl.Card";
+}