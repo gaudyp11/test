muhrizky/tutorials-owl

[SOLUTION] 1.2 Extract counter in a sub component

owl_playground/static/src/counter/counter.js
@@ -1,0 +1,15 @@
+/** @odoo-module */
+
+import { Component, useState } from "@odoo/owl";
+
+export class Counter extends Component {
+    static template = "owl_playground.Counter";
+
+    setup() {
+        this.state = useState({ value: 1 });
+    }
+
+    increment() {
+        this.state.value = this.state.value + 1;
+    }
+}

gaudyp11/test

awesome_owl/static/src/counter/counter.js
@@ -1,0 +1,15 @@
+/** @odoo-module */
+
+import { Component, useState } from "@odoo/owl";
+
+export class Counter extends Component {
+    static template = "awesome_owl.Counter";
+
+    setup() {
+        this.state = useState({ value: 1 });
+    }
+
+    increment() {
+        this.state.value = this.state.value + 1;
+    }
+}